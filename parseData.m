13c24e9a66e6ae9e9b12f66cb41c5acafea507d2
Daniel Smith <714.daniel.smith@gmail.com>
2019-11-15 11:56:48 -0800

    added odds

--- parseData.m
+++ parseData.m
@@ -6,6 +6,8 @@ twenty18 = val.league.seasons(2).games; % this is all of the 2018 data
 game.away = ''
 game.home = ''
 game.outcome = ''
+games.odds_home = 0
+games.odds_away = 0
 
 team.name = ''
 team.elo = 0
@@ -20,7 +22,10 @@ for i = 1:247
     games_list(i).away =  twenty18(i).team_away
     games_list(i).home = twenty18(i).team_home
     games_list(i).outcome = twenty18.outcome
+    games_list(i).odds_home = twenty18.odds_home
+    games_list(i).odds_away = twenty18.odds_draw
     %team_list(i)
+    
     if(get_team_index(games_list(i).away, team_list) == 0)
         team_list(team_list_index).name = games_list(i).away;
         team_list(team_list_index).elo = 2000;
